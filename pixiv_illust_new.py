5b538949354c416d697389ed98b9a6753be2688d
Rusty-starlightExpress <104772568+Rusty-starlightExpress@users.noreply.github.com>
2024-10-05 23:03:36 +0900

    str変換漏れバグ修正

--- pixiv_illust_new.py
+++ pixiv_illust_new.py
@@ -213,7 +213,7 @@ if loop > 1:
                                 print("illust.id    : " + str(illust.id))
                                 print("title_name : " + title_name)
                             #00000.jpgへの対策
-                            file_name = file_name.replace(illust.id+"_p",'{}_{}_p'.format(str(illust.id),title_name))
+                            file_name = file_name.replace(str(illust.id)+"_p",'{}_{}_p'.format(str(illust.id),title_name))
 
                             new_file = os.path.join(saving_direcory_path, file_name)
                             os.rename(frame, new_file)
@@ -235,7 +235,7 @@ if loop > 1:
                                 print("illust.id    : " + str(illust.id))
                                 print("title_name : " + title_name)
                             #00000.jpgへの対策
-                            file_name = file_name.replace(illust.id+"_p",'{}_{}_p'.format(str(illust.id),title_name))
+                            file_name = file_name.replace(str(illust.id)+"_p",'{}_{}_p'.format(str(illust.id),title_name))
 
                             if debug:
                                 print("save_file_name: " + file_name)
